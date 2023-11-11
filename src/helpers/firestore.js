aa5b580ded171fbc33cd269a070ff04cd6a3c4c7
thianparallel <thian.lopez@parallelpublicworks.com>
2023-11-11 12:28:38 -0500

    For deploying

--- src/helpers/firestore.js
+++ src/helpers/firestore.js
@@ -3,13 +3,13 @@ import "firebase/storage";
 
 try {
   firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   });
 } catch (err) {
   if (!/already exists/.test(err.message)) {
